3438f411df9de9d5029b754669639305528401ec
skysowo <louis.poustis-medoubeigbeder@epita.fr>
2024-12-03 17:12:29 +0100

    final

---
+++ src/main/java/fr/lernejo/logger/ContextualLogger.java
@@ -0,0 +1,23 @@
+package fr.lernejo.logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ContextualLogger implements Logger {
+    private final Logger delegateLogger;
+    private final String callerClass;
+    private final DateTimeFormatter formatter;
+
+    public ContextualLogger(String callerClass, Logger delegateLogger) {
+        this.callerClass = callerClass;
+        this.delegateLogger = delegateLogger;
+        this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    }
+
+    @Override
+    public void log(String message) {
+        String enrichedMessage = LocalDateTime.now().format(formatter) + " " + callerClass + " " + message;
+        delegateLogger.log(enrichedMessage);
+    }
+}
+
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,17 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
-    public static Logger getLogger(String name) {
-        return new ConsoleLogger();
+    public static Logger getLogger(String name) { 
+        Logger consoleLogger = new ConsoleLogger();
+        Logger contextualConsoleLogger = new ContextualLogger(name, consoleLogger);
+        Logger fileLogger = new FileLogger("target/captain.log");
+        Predicate<String> simulationFilter = message -> message.contains("Simulation");
+        Logger filteredFileLogger = new FilteredLogger(new ContextualLogger(name, fileLogger), simulationFilter);
+
+       
+        return new CompositeLogger(contextualConsoleLogger, filteredFileLogger);
     }
 }
 
